735713b3504ed1678c9a11bd38a7092d9dbd6509
LiuYun <iwtmbtly@gmail.com>
2022-08-01 16:22:43 +0800

    Pythontip

---
+++ Pythontip算法题/光棍的悲伤.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@author: liuyang
+@software: PyCharm
+@file: 光棍的悲伤.py
+@time: 2022/8/1 16:13
+"""
+a = 7
+print(list(bin(a)[2:]).count('1'))  # 给你一个整数a，数出a在二进制表示下1的个数，并输出
