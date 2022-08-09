000f9b41c6ce3caf7a8fb6588a21467ee20f98ce
LiuYun <iwtmbtly@gmail.com>
2022-08-09 13:37:20 +0800

    Pythontip

---
+++ Pythontip算法题/单身情歌.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: liuyang
+@software: PyCharm
+@file: 单身情歌.py
+@time: 2022/8/9 13:29
+"""
+a = "OurWorldIsFullOfLOVE"
+a = a.upper()
+b = "LOVE"
+if b in a:
+    print("LOVE")
+else:
+    print("SINGLE")
