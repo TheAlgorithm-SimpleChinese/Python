41af7971ecdfacc95e7d470aca9016d35ce78c1d
LiuYun <iwtmbtly@gmail.com>
2022-08-16 16:36:44 +0800

    Pythontip

--- Pythontip算法题/序列判断.py
+++ Pythontip算法题/序列判断.py
@@ -5,3 +5,19 @@
 @file: 序列判断.py
 @time: 2022/8/14 19:39
 """
+L = [1, 1, 3, 3, 4]
+i = 0
+j = 0
+while (i < len(L) - 1) and (j < len(L) - 1):
+    if L[i] <= L[i+1]:
+        i += 1
+    if L[j] >= L[j+1]:
+        j += 1
+
+if i == len(L) - 1:
+    print("UP")
+elif j == len(L) - 1:
+    print("DOWN")
+else:
+    print("WRONG")
+
