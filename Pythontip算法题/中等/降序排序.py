cb9cc669c9cfc3f80711d3b5741cd02480e5dad1
LiuYun <iwtmbtly@gmail.com>
2022-10-07 22:57:18 +0800

    Pythontip:降序排序

---
+++ Pythontip算法题/中等/降序排序.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author: liuyang
+@software: PyCharm
+@file: 降序排序.py
+@time: 2022/10/7 22:50
+"""
+L = [4, 2, 25, 7777777, 100, 3, 77777777, 77777777, 77777777, 77777777]
+
+print(sorted(L, reverse=True))
