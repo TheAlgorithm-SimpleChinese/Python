d54456c41ece312cf8ae9c12f6ab3c0734db9e54
LiuYun <iwtmbtly@gmail.com>
2022-11-26 23:39:58 +0800

    Pythontip：计算两点间的距离

---
+++ Pythontip算法题/中等/计算两点间的距离.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+@author: liuyang
+@software: PyCharm
+@file: 计算两点间的距离.py
+@time: 2022/11/26 23:38
+"""
+x1 = 0
+y1 = 0
+x2 = 0
+y2 = 1
+print("{:.2f}".format(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5))
